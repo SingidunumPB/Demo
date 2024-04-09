4d1af6f078fff580f4e90c38588aea0337a03c6f
simpel-transport-pb <pbisevac@singimail.rs>
2024-04-09 11:52:55 +0200

    .

--- Demo.Domain/Entities/Product.cs
+++ Demo.Domain/Entities/Product.cs
@@ -7,7 +7,6 @@ public class Product
     public Guid Id { get; private set; }
     public string Name { get; private set; }
     public string Description { get; private set; }
-    public Category Category { get; private set; }
     public Company Company { get; private set; }
 
     private Product() { }
