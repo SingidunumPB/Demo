6fbea45056c5d845fe4453c277ed6cd5bd56b39a
simpel-transport-pb <petarbisevac.pb@outlook.com>
2024-04-09 04:40:10 +0200

    .
    .

--- Demo.Application/obj/Debug/net8.0/Demo.Application.AssemblyInfo.cs
+++ Demo.Application/obj/Debug/net8.0/Demo.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39b5d5669b044808302413be8d4f4efd62aac54d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7380159e378994f580aa7f952cb7d3fd453fcf58")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
