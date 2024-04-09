90aa89ea5022abe29448f1be9f42c6a26f0b8553
simpel-transport-pb <petarbisevac.pb@outlook.com>
2024-04-09 04:28:47 +0200

    .
    .

--- Demo.Infrastructure/DependencyInjection.cs
+++ Demo.Infrastructure/DependencyInjection.cs
@@ -14,9 +14,12 @@ public static class DependencyInjection
         var dbConfiguration = new PostgresDbConfiguration();
         configuration.GetSection("PostgresDbConfiguration").Bind(dbConfiguration);
 
-        services.AddDbContext<DemoDbContext>(options =>
-            options.UseNpgsql(dbConfiguration.ConnectionString,
-                x => x.MigrationsAssembly(typeof(DemoDbContext).Assembly.FullName)));
+        if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") != "Test")
+        {
+            services.AddDbContext<DemoDbContext>(options =>
+                options.UseNpgsql(dbConfiguration.ConnectionString,
+                    x => x.MigrationsAssembly(typeof(DemoDbContext).Assembly.FullName)));
+        }
         
         services.AddScoped<IDemoDbContext>(provider => provider.GetRequiredService<DemoDbContext>());
         
